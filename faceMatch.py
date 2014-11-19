245bb76ac2f61129d455578e94f74ab89e9141af
Alexandre Vallette <vallettea@gmail.com>
2014-11-19 16:43:05 +0100

    forgot files

---
+++ faceMatch.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 13 11:01:58 2014
+
+@author: babou
+"""
+
+import cv2
+import pandas as pd
+import os
+import glob
+import time
+import datetime
+import picamera
+import plotly.plotly as py
+import plotly.tools as tls
+from plotly.graph_objs import *
+import matplotlib
+# Force matplotlib to not use any Xwindows backend.
+# to avoid an error : "no display name and no $DISPLAY environment variable"
+matplotlib.use('Agg')
+import pylab as plt
+from pylab import rcParams
+from matplotlib.patches import Rectangle
+
+
+def initFaceRecog():
+
+    # init Cam
+    camera = picamera.PiCamera()
+    camera.vflip = True
+
+    # Init Model
+    node_cascade = cv2.CascadeClassifier('model/cascade.xml')
+
+
+def cam_photo():
+    camera.start_preview()
+    time.sleep(5)
+    timecam = time.strftime("%Y%m%d-%H%M%S") + ".jpg"
+    camera.capture('image_test/' + '%s' %(timecam))
+    time.sleep(115)
+    camera.stop_preview()
+    return(timecam)
+    
+def analyse_photo(filename):
+    img = cv2.imread('image_test/' + filename)
+    resized_image = cv2.resize(img, (640, 480))
+    nodes = node_cascade.detectMultiScale(resized_image)
+    return(len(nodes))
+
+def create_photo_analyse(filename):
+    img = cv2.imread('image_test/' + filename)
+    resized_image = cv2.resize(img, (640, 480))
+    plt.imshow(resized_image)
+    currentAxis = plt.gca()
+    nodes = node_cascade.detectMultiScale(resized_image)
+    xx,yy = [],[]
+    for (x,y,w,h) in nodes:
+        xx += [x+w/2.]
+        yy += [y+h/2.]
+        coords = (x, y), w, h
+        currentAxis.add_patch(Rectangle(*coords, fill=True, alpha=0.2, color='#00FF00', edgecolor='#00FF00', linewidth=3))
+    
+    plt.scatter(xx,yy, color="r")
+    plt.savefig('image_analyse/' + filename)
+    
+def snapAndAnalyse():
+    filename = cam_photo()
+    result = analyse_photo(filename)
+    os.remove('image_test/' + filename)
+    return result
+    
---
+++ stream.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+import plotly.plotly as py  
+import plotly.tools as tls   
+from plotly.graph_objs import *
+import datetime 
+import time
+import json
+import argparse
+from sniff import count_devices
+from faceMatch import initFaceRecog, snapAndAnalyse
+
+# parse command line args
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--interface", required = True, help = "From which interface ar you connected.")
+ap.add_argument("-s", "--server", required = True, help = "What server should be used.")
+ap.add_argument("-r", "--remove", default="", required = False, help = "Devices to ignore seaprated by ; eg: 'Hewlett Packard;FREEBOX SA'")
+args = vars(ap.parse_args())
+
+# init hardware
+initFaceRecog()
+
+# check for past data stored in a file
+try:
+    with open("data/dump.json", "r") as dump:
+        past_data = json.loads(dump.read())
+except:
+    print "No past data"
+    past_data = {"x": [], "y1": [], "y2": [], "y3": [], "y4": []}
+
+
+# init plotly stream
+stream_ids = tls.get_credentials_file()['stream_ids']
+trace1 = Scatter(
+    x=past_data["x"],
+    y=past_data["y1"],
+    mode='lines+markers',
+    name='Total',
+    stream=Stream(token=stream_ids[0], maxpoints=80)
+)
+trace2 = Scatter(
+    x=past_data["x"],
+    y=past_data["y2"],
+    mode='lines+markers',
+    name='Apple',
+    stream=Stream(token=stream_ids[1], maxpoints=80)
+)
+trace3 = Scatter(
+    x=past_data["x"],
+    y=past_data["y2"],
+    mode='lines+markers',
+    name='Other',
+    stream=Stream(token=stream_ids[2], maxpoints=80)
+)
+trace4 = Scatter(
+    x=past_data["x"],
+    y=past_data["y4"],
+    mode='lines+markers',
+    name='Faces',
+    stream=Stream(token=stream_ids[3], maxpoints=80)
+)
+data = Data([trace1, trace2, trace3, trace4])
+layout = Layout(title='Affluence')
+fig = Figure(data=data, layout=layout)
+unique_url = py.plot(fig, filename='LeNode')
+s1 = py.Stream(stream_ids[0])
+s1.open()
+s2 = py.Stream(stream_ids[1])
+s2.open()
+s3 = py.Stream(stream_ids[2])
+s3.open()
+s4 = py.Stream(stream_ids[3])
+s4.open()
+
+ 
+# stream data
+
+while True:
+    
+    x = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+    res = count_devices(args["interface"], args["server"], args["remove"].split(";"))
+    try:
+        faces = snapAndAnalyse()
+    except:
+        print "Error in snapAndAnalyse"
+        faces = 0
+
+    total = sum(res.values())
+    apple = sum(map(lambda x: res[x], filter(lambda x: "apple" in x.lower() , res.keys())))
+    others = sum(map(lambda x: res[x], filter(lambda x: "apple" not in x.lower() , res.keys())))
+
+    past_data["x"] += [x]
+    past_data["y1"] += [total]
+    past_data["y2"] += [apple]
+    past_data["y3"] += [others]
+    past_data["y4"] += [faces]
+    print past_data
+    with open("data/dump.json", "w") as dump:
+        dump.write(json.dumps(past_data))
+    
+    
+    s1.write(dict(x=x, y=total))  
+    s2.write(dict(x=x, y=apple))  
+    s3.write(dict(x=x, y=others))  
+    s4.write(dict(x=x, y=faces))  
+
+    time.sleep(30)
+    print x
+    print res    
+
+
+
