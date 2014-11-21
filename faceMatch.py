da3f14d284285142933b1d78c9d182bf03605a4d
Armand Gilles <arm.gilles@gmail.com>
2014-11-21 10:34:39 +0100

    fix bug on figure

--- faceMatch.py
+++ faceMatch.py
@@ -53,8 +53,8 @@ def analyse_photo(filename, node_cascade):
 def create_photo_analyse(filename, node_cascade):
     img = cv2.imread('image_test/' + filename)
     resized_image = cv2.resize(img, (640, 480))
-    plt.fig
-    plt.imshow(resized_image)
+    fig = plt.figure()
+    fig.imshow(resized_image)
     currentAxis = plt.gca()
     nodes = node_cascade.detectMultiScale(resized_image)
     xx,yy = [],[]
@@ -64,8 +64,8 @@ def create_photo_analyse(filename, node_cascade):
         coords = (x, y), w, h
         currentAxis.add_patch(Rectangle(*coords, fill=True, alpha=0.2, color='#00FF00', edgecolor='#00FF00', linewidth=3))
     
-    plt.scatter(xx,yy, color="r")
-    plt.savefig('image_analyse/' + filename)
+    fig.scatter(xx,yy, color="r")
+    fig.savefig('image_analyse/' + filename)
     
 def snapAndAnalyse(camera, node_cascade):
     filename = cam_photo(camera)
