9a348cd01c3475a8a9a42638db471a5c92dea37b
Armand Gilles <arm.gilles@gmail.com>
2014-12-16 17:13:28 +0100

    build fonction

--- temperature_humidity.py
+++ temperature_humidity.py
@@ -7,16 +7,14 @@ Created on Tue Dec 16 16:55:10 2014
 
 import Adafruit_DHT
 
-sensor = Adafruit_DHT.DHT11
 
-# connected to pin 23
-pin = 4
 
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-
-if humidity is not None and temperature is not None:
-        print 'Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity)
-        print "Temperature= " + str(temperature)
-        print "Humidity= " + str(humidity)
-else:
-        print 'Failed to get reading. Try again!'
+def meteo_node():
+    
+    sensor = Adafruit_DHT.DHT11
+    
+    # connected to pin 23
+    pin = 4
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    
+    return (humidity, temperature)
